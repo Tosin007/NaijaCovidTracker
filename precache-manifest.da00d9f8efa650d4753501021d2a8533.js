796ce6bd00ac62dcc5a6e0a3035907353b0a7ba6
Tosin Arowosegbe <tosin100arowosegbe@gmail.com>
2020-07-16 02:59:58 +0100

    Updates

---
+++ precache-manifest.da00d9f8efa650d4753501021d2a8533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6691b48b6ce433238e745892ccfa9f31",
+    "url": "/NaijaCovidTracker/index.html"
+  },
+  {
+    "revision": "546ba84818260519e680",
+    "url": "/NaijaCovidTracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5be9253e756197ccbb02",
+    "url": "/NaijaCovidTracker/static/js/2.98805c59.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/NaijaCovidTracker/static/js/2.98805c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546ba84818260519e680",
+    "url": "/NaijaCovidTracker/static/js/main.6610fa68.chunk.js"
+  },
+  {
+    "revision": "08f56ec9ae1a32807243",
+    "url": "/NaijaCovidTracker/static/js/runtime-main.c7a3aea5.js"
+  }
+]);
